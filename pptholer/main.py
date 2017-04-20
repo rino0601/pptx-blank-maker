3d4ab3f6ce92a694e38d41a7554eba2df3996efc
HanJongKo <rino0601.dev@gmail.com>
2017-04-20 21:04:57 +0900

    support dir path

--- pptholer/main.py
+++ pptholer/main.py
@@ -1,5 +1,7 @@
 # coding=utf-8
+import glob
 import io
+import os
 
 import click
 from pptx import Presentation
@@ -10,32 +12,36 @@ __author__ = 'rino0601'
 @click.command()
 @click.argument('source_pptx_file_path', type=click.Path(exists=True))
 def cli(source_pptx_file_path):
-    click.echo(u"Welcome, Before we start converting, There is some things you have to know.")
-    click.echo(u"   1. This tool does not support videos which you upload tistory directly.\n"
-               u"   They seems only play able on tistory blog.\n"
-               u"   If you have good idea for this, please contribute this project.\n"
-               u"   2. This tool ignore comments, track backs, tags and categories. \n"
-               u"   There is some different with jekyll and tistory's system.\n"
-               u"   I didn't have enough time for consider that.\n"
-               u"   3. If you want bring your picture and attachment from tistory, that blog has to be alive.\n"
-               u"   (backup_with_attachment.xml isn't available for now.)\n")
+    origin_dir = os.getcwd()
+    click.echo(u"Start converting...")
     # click.confirm(u'Do you want to continue?', abort=True)
     # with click.progressbar(posts, label=u"Converting posts...") as bar:
-    file_path = click.format_filename(source_pptx_file_path)
-    with io.open(file_path, 'rb') as source_pptx_file:
-        prs = Presentation(source_pptx_file)
-        with io.open(file_path + '.txt', 'w', encoding='utf8') as notepad:
-            for s_num, slide in enumerate(prs.slides):
-                for shape in slide.shapes:
-                    if not shape.has_text_frame:
-                        continue
-                    for i, paragraph in enumerate(shape.text_frame.paragraphs):
-                        text_row = "".join([run.text for run in paragraph.runs])
-                        print(s_num, i, text_row)
-                        notepad.write(text_row)
-                        notepad.write('\n')
 
-                        # for run in paragraph.runs:
-                        #     run.text += "ㅗ"
+    filename = click.format_filename(source_pptx_file_path)
+    file_path_list = [filename]
+    if os.path.isdir(filename):
+        click.echo(u"Searching *.pptx files in given directory...")
+        os.chdir(source_pptx_file_path)
+        file_path_list = glob.glob('*.pptx')
+        click.echo(file_path_list)
 
+    for file_path in file_path_list:
+        click.echo(u"Processing %r ..." % file_path)
+        with io.open(file_path, 'rb') as source_pptx_file:
+            prs = Presentation(source_pptx_file)
+            with io.open(file_path + '.txt', 'w', encoding='utf8') as notepad:
+                for s_num, slide in enumerate(prs.slides):
+                    for shape in slide.shapes:
+                        if not shape.has_text_frame:
+                            continue
+                        for i, paragraph in enumerate(shape.text_frame.paragraphs):
+                            text_row = "".join([run.text for run in paragraph.runs])
+                            # click.echo(s_num, i, text_row)
+                            notepad.write(text_row)
+                            notepad.write('\n')
+
+                            # for run in paragraph.runs:
+                            #     run.text += "ㅗ"
+
+    os.chdir(origin_dir)
     click.echo("DONE")
